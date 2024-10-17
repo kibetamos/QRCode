1803e531e7b39630cb396b05717ec01c78c2dee1
Kibet Amos <amos.rotich@strathmore.edu>
2024-10-17 11:20:55 +0300

    added backend

--- qr_event_system/qr_event_system/urls.py
+++ qr_event_system/qr_event_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend project.
+URL configuration for qr_event_system project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- qr_event_system/qr_event_system/wsgi.py
+++ qr_event_system/qr_event_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for backend project.
+WSGI config for qr_event_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_event_system.settings')
 
 application = get_wsgi_application()
