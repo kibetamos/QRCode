3f1c2a8d450761078b6ecbcacc6e96ba4814ed47
Kibet Amos <amos.rotich@strathmore.edu>
2024-10-17 11:28:04 +0300

    add installed libraries

--- qr_event_system/qr_event_system/settings.py
+++ qr_event_system/qr_event_system/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'events',
+    'allauth',
+    'dj_rest_auth',
 ]
 
 MIDDLEWARE = [
